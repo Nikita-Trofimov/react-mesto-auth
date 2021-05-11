e1f09c65859c4c3de58d15f4a7d5f390583204b1
Nikita Trofimov <nikita.igorevi4@yandex.ru>
2021-05-11 18:18:14 +0300

    изменены названия переменных в компоненте InfoTooltip

--- src/components/InfoTooltip.js
+++ src/components/InfoTooltip.js
@@ -1,10 +1,10 @@
-import registration_ok from '../images/registration_ok.svg'
-import registration_err from '../images/registration_err.svg'
+import registrationOk from '../images/registration_ok.svg'
+import registrationErr from '../images/registration_err.svg'
 function InfoTooltip({isOpen, onClose, isRegistration}) {
   return (
     <section className={`popup popup-illustration-container ${isOpen && 'popup_opened'}`}>
       <div className="popup__container">
-        <img className="info-tooltip__img" alt="Ошибка" src={isRegistration ? registration_ok : registration_err}/>
+        <img className="info-tooltip__img" alt="Ошибка" src={isRegistration ? registrationOk : registrationErr}/>
         <p className="info-tooltip__text">{isRegistration ? "Вы успешно зарегистрировались!" : "Что-то пошло не так!Попробуйте ещё раз."}</p>
         <button type="button" className="popup__close-button" onClick={onClose}></button>    
       </div>
