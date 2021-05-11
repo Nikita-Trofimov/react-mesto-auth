b21c25b3b3f9fe2657002ce82be29b06d86a6d3e
Nikita Trofimov <nikita.igorevi4@yandex.ru>
2021-05-11 18:25:26 +0300

    удален лишний import из ProtectedRoute

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
-import Footer from "./Footer";
 
 function ProtectedRoute({ component: Component, ...props }) {
   return (
